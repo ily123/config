c45e490e4fbf47c32e5d14d5c19930a5d152f043
Ilya Novikov <ibnovikov@gmail.com>
2024-01-14 16:31:45 -0600

    Fixed Lazy naming conflict.

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,1 +1,10 @@
-require('settings')
+-- Load native settings
+require("settings")
+require("keymap")
+
+-- Install the Lazy package manager
+require("lazy_init")
+
+-- Using Lazy package manager, load third party plugins
+-- Any lua file in ~/.config/nvim/lua/plugins/*.lua will be automatically merged in the main plugin spec
+require("lazy").setup("plugins")
