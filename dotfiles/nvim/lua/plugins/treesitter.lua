bd0b531d3bd6247fc5e0b08b0b5e38f34a0513de
Ilya Novikov <ibnovikov@gmail.com>
2024-01-14 20:18:34 -0600

    Add treesitter.

---
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require'nvim-treesitter.configs'.setup {
+      ensure_installed = {"typescript", "python", "vim", "lua", "hcl"},
+      sync_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      }
+    }
+  end
+}
