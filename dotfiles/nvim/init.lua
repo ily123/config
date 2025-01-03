b2637bca486ba4cf9ec7628bc072b87c91ff7e09
ilya <ibnovikov@gmail.com>
2025-01-02 21:22:13 -0600

    Adjust setup command for lazy.

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -8,7 +8,12 @@ require("config.lazy")
 -- Using Lazy package manager, load third party plugins
 -- Any lua file in ~/.config/nvim/lua/plugins/*.lua will be
 -- automatically merged in the main plugin spec
-require("lazy").setup("plugins")
+require("lazy").setup({
+  spec = {
+    import = "plugins"
+  },
+  checker = { enabled = true },
+})
 
 -- LSP requires extensive configuration that can't be inlined
 -- so we call it in a separate script
