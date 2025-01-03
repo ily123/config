ab3a3ebe6cac8a82738e6a8516ac62a11907be81
ilya <ibnovikov@gmail.com>
2025-01-02 21:09:59 -0600

    Rearrange struct of neovim config.

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,9 +1,9 @@
 -- Load native settings
-require("settings")
-require("keymap")
+require("config.settings")
+require("config.keymap")
 
 -- Install the Lazy package manager
-require("lazy_init")
+require("config.lazy")
 
 -- Using Lazy package manager, load third party plugins
 -- Any lua file in ~/.config/nvim/lua/plugins/*.lua will be
@@ -12,4 +12,4 @@ require("lazy").setup("plugins")
 
 -- LSP requires extensive configuration that can't be inlined
 -- so we call it in a separate script
-require("lsp_config")
+require("config.lsp")
