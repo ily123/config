68858188af204ec9111223ef9294bc3ffe74c046
ilya <ibnovikov@gmail.com>
2025-01-15 23:07:17 -0600

    format

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -1,21 +1,20 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",  -- required
-      "nvim-telescope/telescope-fzf-native.nvim",  -- faster sorting
-      "BurntSushi/ripgrep",  -- faster grep
-      "sharkdp/fd",  -- faster find
-    },
-    config = function()
-      local opts = {noremap = true, silent = true}
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",                    -- required
+    "nvim-telescope/telescope-fzf-native.nvim", -- faster sorting
+    "BurntSushi/ripgrep",                       -- faster grep
+    "sharkdp/fd",                               -- faster find
+  },
+  config = function()
+    local opts = { noremap = true, silent = true }
 
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
-      vim.keymap.set('n', '<leader>fs', builtin.current_buffer_fuzzy_find, opts)
-      vim.keymap.set('n', '<leader>?', builtin.oldfiles, opts)
-    end,
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
+    vim.keymap.set('n', '<leader>fs', builtin.current_buffer_fuzzy_find, opts)
+    vim.keymap.set('n', '<leader>?', builtin.oldfiles, opts)
+  end,
 }
-
