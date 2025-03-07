4980d66affe45653bfecdeda0e42030f196772a3
ilya <ibnovikov@gmail.com>
2025-03-06 19:57:03 -0600

    Add comment on clipboard provider

--- dotfiles/nvim/lua/config/settings.lua
+++ dotfiles/nvim/lua/config/settings.lua
@@ -3,15 +3,19 @@ local set = vim.opt
 -- Misc
 set.backspace = { "indent", "eol", "start" }  -- Enable backspace
 set.cmdheight = 2
-set.signcolumn = "yes"  -- Always show sign column
+set.signcolumn = "yes"                        -- Always show sign column
 set.number = true
-set.statusline = set.statusline:get() .. "%F"  -- Show file path
-set.clipboard = "unnamedplus"  -- Yank now also yanks to system clipboard
+set.statusline = set.statusline:get() .. "%F" -- Show file path
+
+-- Clipboard
+-- This needs a provider to work:
+-- https://askubuntu.com/a/1486875
+set.clipboard = "unnamedplus" -- Yank now also yanks to system clipboard
 
 -- Tab Options
 set.shiftwidth = 2
 set.tabstop = 2
-set.softtabstop = 2  -- Number of spaces a tab counts when editing
+set.softtabstop = 2 -- Number of spaces a tab counts when editing
 set.expandtab = true
 
 -- Folding options
