ff30c3bd4f5ee6ff7e9f5885b03f64a9ceb0ca27
ilya <ibnovikov@gmail.com>
2026-03-06 23:32:40 -0600

    Remove icons.

--- v2/config.d/nvim/init.lua
+++ v2/config.d/nvim/init.lua
@@ -1,8 +1,8 @@
 -- Built-in settings like keybinds
 require("settings")
 
--- LSP configs
-require("lsp")
-
 -- Bootstrap lazy package manager
 require("lazy_bootstrap")
+
+-- LSP configs
+require("lsp")
--- v2/config.d/nvim/lua/plugins/blink.lua
+++ v2/config.d/nvim/lua/plugins/blink.lua
@@ -25,7 +25,14 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = { 
+      documentation = { auto_show = false },
+      menu = {
+        draw = {
+          columns = { {'label', 'label_description', gap = 1 }, { 'kind'} }
+        }
+      }
+    },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
